12cfbdb1262193f0c90950640a6dabdfb9ced401
musa <lucas@ruptiva.com>
2019-04-09 00:46:02 -0300

    [Ruby] Enumerable collect

---
+++ ruby/medium/enumerable-collect.rb
@@ -0,0 +1,6 @@
+def rot13(secret_messages)
+  input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+
+  secret_messages.collect{ |s| s.tr(input, output) }
+end
