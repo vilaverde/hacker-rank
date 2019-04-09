ae8981392ecc332e0b08a6a1edba4b1a4fc11087
musa <lucas@ruptiva.com>
2019-04-09 00:47:28 -0300

    [Ruby] Enumerables any? all? none? find?

---
+++ ruby/medium/enumerables-any-all-none-find.rb
@@ -0,0 +1,19 @@
+def func_any(hash)
+  hash.any?{ |k, _| k.is_a?(Integer) }
+end
+
+def func_all(hash)
+  hash.all?{ |_, v| v.is_a?(Integer) && v < 10 }
+end
+
+def func_none(hash)
+  hash.none?{ |_, v| v.nil? }
+end
+
+def func_find(hash)
+  hash.find do |k, v|
+    (k.is_a?(Integer) && v.is_a?(Integer) && v < 20) ||
+      (k.is_a?(String) && v.is_a?(String) && v.start_with?("a"))
+  end
+end
+
